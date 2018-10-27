db078a476acc2f13f632be92141b23e9d25c3f73
sage417 <sage417@foxmail.com>
2018-10-27 12:56:41 +0800

    fix author resolver

--- src/main/java/app/pooi/graphql/demo/graphql/resolver/AuthorResolver.java
+++ src/main/java/app/pooi/graphql/demo/graphql/resolver/AuthorResolver.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 @Component
-public class AuthorResolver implements GraphQLResolver<Post> {
+public class AuthorResolver implements GraphQLResolver<Author> {
 
     private PostDao postDao;
 
